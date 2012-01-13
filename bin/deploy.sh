ee651a200243bc9cce8c42fc5543192d2d029809
Bjorn Remseth <la3lma@gmail.com>
2012-01-13 10:38:28 +0100

    Write a short message to stdout when publishing to dropbox

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -5,6 +5,7 @@ TARGET=$HOME/Desktop/Dropbox/Public/ai-course-notes.pdf
 
 if [ -f "$SOURCE" ] ; then
    cp $SOURCE $TARGET
+   echo "Copied $SOURCE to $TARGET"
 else
    echo "Couldn't find source pdf to deploy: $SOURCE"
 fi
